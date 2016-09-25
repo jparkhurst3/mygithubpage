0ace7a8929c319c9e91ff02dabfcb415e0a49cb6
Joanna Parkhurst <parkhurst.joanna@gmail.com>
2016-09-25 19:55:08 -0400

    First Upload for Portfolio Update - 9/25/16

---
+++ Portfolio_9.25.16/plugins/contact.php
@@ -0,0 +1,10 @@
+<?php $name = $_POST['Name'];
+$email = $_POST['Email'];
+$message = $_POST['Message'];
+$formcontent="From: $name \n Message: $message";
+$recipient = "parkhurst.joanna@gmail.com";
+$subject = "Contact Form";
+$mailheader = "From: $email \r\n";
+mail($recipient, $subject, $formcontent, $mailheader) or die("Oh no! Please try again later.");
+echo "Thank You!";
+?>
